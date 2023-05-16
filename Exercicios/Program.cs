0b257f10eed8a341d6fadfe3a3e9434f9c3a47a3
Alex <oalexsanderm@gmail.com>
2023-05-16 17:01:16 -0300

    Falta Ex 8

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Globalization;
 // Exercícios sobre Estrutura Sequencial (entrada, processamento, saída)
 
-
 #region Ex - 01
 //Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma 
 //mensagem explicativa, conforme exemplos 
@@ -133,6 +132,215 @@ Console.WriteLine($"TRIANGULO: {triangulo.ToString("F3", CultureInfo.InvariantCu
 */
 #endregion
 
-// Futuros Exercicios 
+// Exercícios sobre Estrutura Condicional (if-else)
 
-Console.WriteLine("Olá Mundo!");
+#region Ex - 01
+//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+/*
+Console.WriteLine("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero < 0 )
+{
+    Console.WriteLine("NEGATIVO");
+}
+else
+{
+    Console.WriteLine("NÃO NEGATIVO");
+}
+*/
+#endregion
+
+#region Ex - 02
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpa
+
+/*
+Console.WriteLine("Informe um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+if(numero % 2 == 0 )
+{
+    Console.WriteLine("PAR");
+}
+else 
+{
+    Console.WriteLine("IMPAR");
+}
+*/
+#endregion
+
+#region Ex - 03
+//Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao 
+//Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em 
+//ordem crescente ou decrescente.
+
+/*
+Console.WriteLine("Informe dois número inteiros separados por espaço: ");
+string[] numeros = Console.ReadLine().Split(' ');
+int a = int.Parse(numeros[0]);
+int b = int.Parse(numeros[1]);
+
+if(a % b == 0 || b % a == 0)
+{
+    Console.WriteLine("São Multiplos");
+}
+else
+{
+    Console.WriteLine("Não são Multiplos");
+}
+*/
+#endregion
+
+#region Ex - 04
+//Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode 
+//começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas
+
+/*
+Console.WriteLine("Informe a hora inicial e a hora final do jogo separadas por espaço: ");
+string[] horas = Console.ReadLine().Split(' ');
+int horaInicial = int.Parse(horas[0]);
+int horaFinal = int.Parse(horas[1]);
+int duracao;
+
+if (horaInicial < horaFinal)
+{
+    duracao = horaFinal - horaInicial;   
+} else
+{
+    duracao = 24 - horaInicial + horaFinal;
+}
+
+Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+*/
+#endregion
+
+#region Ex - 05
+//Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A 
+//seguir, calcule e mostre o valor da conta a pagar
+
+/*
+Console.WriteLine("Informe o codigo do item e a quantidade separada por espaço: ");
+string[] pedido = Console.ReadLine().Split(' ');
+int codigo = int.Parse(pedido[0]);
+int quantidade = int.Parse(pedido[1]);
+
+decimal valorConta = 0;
+if (codigo == 1)
+{
+    valorConta = quantidade * 4.0m;
+} 
+else if (codigo == 2)
+{
+    valorConta =  quantidade *4.50m;
+}
+else if (codigo == 3)
+{
+    valorConta = quantidade * 5.00m;
+}
+else if (codigo == 4)
+{
+    valorConta =  quantidade * 2.00m;
+}
+else if (codigo == 5)
+{
+    valorConta =  quantidade * 1.50m;
+}
+else 
+{
+    Console.WriteLine("Código invalido");
+}
+
+Console.WriteLine($"Total: R$ {valorConta.ToString("F2", CultureInfo.InvariantCulture)}");
+*/
+#endregion
+
+#region Ex - 06
+//Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos 
+//seguintes intervalos([0,25], (25,50], (50, 75], (75, 100]) este valor se encontra. Obviamente se o valor não estiver em 
+//nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+
+/*
+Console.WriteLine("Informe um valor: ");
+double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+if(valor >= 0 && valor <= 25)
+{
+    Console.WriteLine("Intervalo [0,25]");
+}
+else if (valor > 25 && valor <= 50)
+{
+    Console.WriteLine("Intervalo [25,50]");
+}
+else if (valor > 50 && valor <= 75)
+{
+    Console.WriteLine("Intervalo [05,75]");
+}
+else if (valor > 75 && valor <= 100)
+{
+    Console.WriteLine("Intervalo [75,100]");
+}
+else
+{
+    Console.WriteLine("Fora de intervalo");
+}
+*/
+#endregion
+
+#region Ex - 07
+//Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas 
+//de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o 
+//ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+//Se o ponto estiver na origem, escreva a mensagem “Origem”.
+//Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação
+
+/*
+Console.WriteLine("Informe 2 valores (x e y) separados por espaço: ");
+string[] valores = Console.ReadLine().Split(' ');
+double x = double.Parse(valores[0]);
+double y = double.Parse(valores[1]);
+
+ if (x == 0 && y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if (x == 0 )
+{
+    Console.WriteLine("Eixo Y");
+} 
+else if(y == 0 )
+{
+    Console.WriteLine("Eixo X");
+}
+else if(x > 0 && y > 0)
+{
+    Console.WriteLine("Q1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Q2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Q3");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Q4");
+}
+*/
+#endregion
+
+#region Ex - 08
+//Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem 
+//que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem 
+//qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+//Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e 
+//mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+//Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de 
+//salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é 
+//de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com 
+//duas casas decimais
+
+
+#endregion
